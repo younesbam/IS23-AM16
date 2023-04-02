40723d40ad3152e27b55d4b8486460e141a2ec7c
nicologandini <126022150+nicologandini@users.noreply.github.com>
2023-04-02 19:24:13 +0200

    Update Card.java

--- src/main/java/it/polimi/ingsw/model/cards/Card.java
+++ src/main/java/it/polimi/ingsw/model/cards/Card.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.model.cards;
 
 /**
  * Abstract class that represent a generic Card type
+ * @author Nicolo' Gandini
  */
 public abstract class Card {
     private int cardNumber;
