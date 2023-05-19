faa41ae7c63944d6705381f23db8c2c27523034c
Younes Bamhaoud <younes.bamhaoud@mail.polimi.it>
2023-05-19 18:51:09 +0200

    JavaFX Lib added, pom updated, gui resources added

---
+++ src/main/java/it/polimi/ingsw/client/CLI.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.client;
+
+import java.util.Scanner;
+
+public class CLI {
+
+    private final Scanner input;
+
+
+}
--- src/main/java/it/polimi/ingsw/client/gui/GUI.java
+++ src/main/java/it/polimi/ingsw/client/gui/GUI.java
@@ -1,10 +1,24 @@
 package it.polimi.ingsw.client.gui;
 
 import java.beans.PropertyChangeListener;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class GUI {
+import java.io.IOException;
+
+public class GUI extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("joinScene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("MyShelfie");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
-
+        launch();
     }
 }
