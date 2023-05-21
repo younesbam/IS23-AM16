1541ea28795abac7abf8fcb8cb88fe15376eac36
Younes Bamhaoud <younes.bamhaoud@mail.polimi.it>
2023-05-21 13:12:53 +0200

    loader updated

--- src/main/java/it/polimi/ingsw/client/gui/GUI.java
+++ src/main/java/it/polimi/ingsw/client/gui/GUI.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class GUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("joinScene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("/fxml/joinScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("MyShelfie");
         stage.setScene(scene);
