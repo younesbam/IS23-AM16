4ed93882d6f731b86077254ea0e64240485af973
nicologandini <nicolo.gandini@mail.polimi.it>
2023-05-30 01:13:08 +0200

    input check in client and coordinates added

--- src/main/java/it/polimi/ingsw/communications/clientmessages/actions/PickTilesAction.java
+++ src/main/java/it/polimi/ingsw/communications/clientmessages/actions/PickTilesAction.java
@@ -1,42 +1,24 @@
 package it.polimi.ingsw.communications.clientmessages.actions;
 
+import it.polimi.ingsw.common.Coordinate;
+
+import java.util.List;
+
+/**
+ * Pick tiles request from the player
+ */
 public class PickTilesAction implements GameAction{
 
-    private int[][] coordinates;
-
-    public PickTilesAction(int row1, int col1){
-        coordinates = new int[1][2];
-
-        coordinates[0][0] = row1;
-        coordinates[0][1] = col1;
-    }
-
-    public PickTilesAction(int row1, int col1, int row2, int col2){
-        coordinates = new int[2][2];
-
-        coordinates[0][0] = row1;
-        coordinates[0][1] = col1;
-        coordinates[1][0] = row2;
-        coordinates[1][1] = col2;
-
-    }
-
-    public PickTilesAction(int row1, int col1, int row2, int col2, int row3, int col3){
-        coordinates = new int[3][2];
-
-        coordinates[0][0] = row1;
-        coordinates[0][1] = col1;
-        coordinates[1][0] = row2;
-        coordinates[1][1] = col2;
-        coordinates[2][0] = row3;
-        coordinates[2][1] = col3;
-    }
-
-
     /**
-     * Tiles getter.
+     * Coordinates of the selected tiles
      */
-    public int[][] getTiles(){
-        return this.coordinates;
+    List<Coordinate> coordinates;
+
+    public PickTilesAction(List<Coordinate> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public List<Coordinate> getCoordinates() {
+        return coordinates;
     }
 }
