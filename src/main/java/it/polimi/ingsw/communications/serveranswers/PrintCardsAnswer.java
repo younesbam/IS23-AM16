cbaf18d7959580e860f7502d65870474ad7b369c
Younes Bamhaoud <younes.bamhaoud@mail.polimi.it>
2023-06-08 23:26:57 +0200

    cli endGame added

---
+++ src/main/java/it/polimi/ingsw/communications/serveranswers/PrintCardsAnswer.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.communications.serveranswers;
+
+public class PrintCardsAnswer implements Answer{
+
+    @Override
+    public Object getAnswer() {
+        return null;
+    }
+}
