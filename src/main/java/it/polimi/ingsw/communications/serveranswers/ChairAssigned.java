21f7126980ba5e5016eb7210016813bf28e76a3c
Nicolo Gandini <126022150+nicologandini@users.noreply.github.com>
2023-06-10 21:16:10 +0200

    Merge pull request #20 from younesbam/resilience
    Resilience implemented

--- src/main/java/it/polimi/ingsw/communications/serveranswers/ChairAssigned.java
+++ src/main/java/it/polimi/ingsw/communications/serveranswers/ChairAssigned.java
@@ -1,6 +1,13 @@
-package it.polimi.ingsw.communications.serveranswers;
+package it.polimi.ingsw.communications.serveranswers.info;
 
+<<<<<<<< HEAD:src/main/java/it/polimi/ingsw/communications/serveranswers/ChairAssigned.java
 public class ChairAssigned implements Answer{
+========
+import it.polimi.ingsw.communications.serveranswers.Answer;
+
+public class EndOfYourTurn implements Answer {
+
+>>>>>>>> 03a2833 (Merge pull request #20 from younesbam/resilience):src/main/java/it/polimi/ingsw/communications/serveranswers/info/EndOfYourTurn.java
     private String answer;
 
     public ChairAssigned(){
--- src/main/java/it/polimi/ingsw/communications/serveranswers/GameReady.java
+++ src/main/java/it/polimi/ingsw/communications/serveranswers/GameReady.java
@@ -1,6 +1,13 @@
-package it.polimi.ingsw.communications.serveranswers;
+package it.polimi.ingsw.communications.serveranswers.info;
 
+<<<<<<<< HEAD:src/main/java/it/polimi/ingsw/communications/serveranswers/GameReady.java
 public class GameReady implements Answer{
+========
+import it.polimi.ingsw.communications.serveranswers.Answer;
+
+public class ItsYourTurn implements Answer {
+
+>>>>>>>> 03a2833 (Merge pull request #20 from younesbam/resilience):src/main/java/it/polimi/ingsw/communications/serveranswers/info/ItsYourTurn.java
     private String answer;
 
     public GameReady(){
